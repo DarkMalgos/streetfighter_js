d4d3ae07df0730915e34c040ac3184acae9079a0
Florian SAUMUROT <florian.saumurot@gmail.com>
2016-11-22 13:42:34 +0100

    menu j && switch select perso

---
+++ script/script.js
@@ -0,0 +1,33 @@
+function show_menu(){
+    var l = document.getElementById('logo');
+    l.style.top = "0";
+    var s = document.getElementById('start');
+    s.style.display = "block";
+    var e = document.getElementById('exit');
+    e.style.display = "block";
+}
+
+var l = document.getElementById("logo");
+l.addEventListener('click', show_menu);
+
+function disable_menu(){
+    var l = document.getElementById('logo');
+    l.style.top = "30%";
+    var s = document.getElementById('start');
+    s.style.display = "none";
+    var e = document.getElementById('exit');
+    e.style.display = "none";
+}
+
+var e = document.getElementById("exit");
+e.addEventListener('click', disable_menu);
+
+function show_select_player(){
+    var l = document.getElementById('menu');
+    l.style.display = "none";
+    var sp = document.getElementById('select_player');
+    sp.style.display = "block";
+}
+
+var s = document.getElementById("start");
+s.addEventListener('click', show_select_player);
