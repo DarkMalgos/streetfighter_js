b499ef5b1f2a328d7db595bf57ef346398b9cf74
Florian SAUMUROT <florian.saumurot@gmail.com>
2016-11-23 17:57:00 +0100

    perso select good

--- script/script.js
+++ script/script.js
@@ -39,10 +39,10 @@ s.addEventListener('click', show_select_player);
 function p_select(nb_perso){
     mp = document.getElementById('message_player');
     if (mp.textContent == "Player 1 select your perso"){
-        console.log("toto");
+        pl1 = nb_perso;
         mp.textContent = "Player 2 select your perso";
     }else{
-        console.log("titi"); 
+        pl2 = nb_perso;
         var sp = document.getElementById('select_player');
         sp.style.display = 'none';
         var ss = document.getElementById('select_stage');
@@ -51,15 +51,48 @@ function p_select(nb_perso){
     console.log(pl1 + ' ' + pl2);
 }
 
+function p1_select(){
+    var nb_perso = 1;
+    p_select(nb_perso);
+}
+
 var p1 = document.getElementById("Ryu");
-p1.addEventListener('click', p_select);
+p1.addEventListener('click', p1_select);
+
+function p2_select(){
+    var nb_perso = 2;
+    p_select(nb_perso);
+}
+
 var p2 = document.getElementById("ChunLi");
-p2.addEventListener('click', p_select);
+p2.addEventListener('click', p2_select);
+
+function p3_select(){
+    var nb_perso = 3;
+    p_select(nb_perso);
+}
+
 var p3 = document.getElementById('Ken');
-p3.addEventListener('click', p_select);
+p3.addEventListener('click', p3_select);
+
+function p4_select(){
+    var nb_perso = 4;
+    p_select(nb_perso);
+}
+
 var p4 = document.getElementById('Guile');
-p4.addEventListener('click', p_select);
+p4.addEventListener('click', p4_select);
+
+function p5_select(){
+    var nb_perso = 5;
+    p_select(nb_perso);
+}
 var p5 = document.getElementById('Akuma');
-p5.addEventListener('click', p_select);
+p5.addEventListener('click', p5_select);
+
+function p6_select(){
+    var nb_perso = 6;
+    p_select(nb_perso);
+}
 var p6 = document.getElementById('DeeJay');
-p6.addEventListener('click', p_select);
+p6.addEventListener('click', p6_select);
