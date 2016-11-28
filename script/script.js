065003d31811540165f187d09e826fd0a5233ae7
Florian SAUMUROT <florian.saumurot@gmail.com>
2016-11-28 11:27:26 +0100

    move select player

--- script/script.js
+++ script/script.js
@@ -12,6 +12,7 @@ var pl1 = 0,
     pl2 = 0,
     stage;
 
+console.log(document.getElementById('logo').offsetTop);
 function show_menu(){
     var l = document.getElementById('logo');
     l.style.top = "0";
@@ -46,88 +47,40 @@ function show_select_player(){
 var s = document.getElementById("start");
 s.addEventListener('click', show_select_player);
 
-function get_off_top( el ) {
-    var _y = 0;
-    while( el && !isNaN( el.offsetTop ) ) {
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
+function right()
+{
+    var _class_selection = document.getElementsByClassName("selection")[0],
+        _id = _class_selection.getAttribute('id'),
+        _id_perso = document.getElementById(_id);
+    _id_perso_next = parseInt(_class_selection.getAttribute('id').charAt(1)) + 1;
+    console.log(_id_perso);
+    if (_id_perso_next <= 6){
+        var _perso_next = document.getElementById('p' + _id_perso_next);
+        _id_perso.classList.remove("selection");
+        _id_perso.classList.add("noselection");
+        _perso_next.classList.remove("noselection");
+        _perso_next.classList.add("selection");
     }
-    return (_y);
-} 
-
-function get_off_left( el ) {
-    var _x = 0;
-    while( el && !isNaN( el.offsetLeft ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        el = el.offsetParent;
-    }
-    return (_x);
-}
-
-/*function down(){
-    var _s = document.getElementById("selection");
-    var _s_top = get_off_top(document.getElementById("selection"));
-    console.log(_s_top);
-    if (_s_top == 232)
-        _s.style.top = "58%";
-}*/
-
-function right(){
-    var _s = document.getElementById("selection");
-    var _s_left = get_off_left(document.getElementById("selection"));
-    console.log(_s_left);
-    if (_s_left < 471)
-        _s.style.left = _s_left + 123 + "px";
-    else if (_s_left == 471)
-        _s.style.left = _s_left + 300 + "px";
-    else if (_s_left < 1011)
-        _s.style.left = _s_left + 120 + "px";
+    
+    
 }
 
 function left(){
-    var _s = document.getElementById("selection");
-    var _s_left = get_off_left(document.getElementById("selection"));
-    console.log(_s_left);
-    if (_s_left > 771)
-        _s.style.left = _s_left - 120 + "px";
-    else if (_s_left == 771)
-        _s.style.left = _s_left - 300 + "px";
-    else if (_s_left > 225)
-        _s.style.left = _s_left - 123 + "px";
-}
-
-function p_select(nb_perso){
-    mp = document.getElementById('message_player');
-    if (mp.textContent == "Player 1 select your perso"){
-        pl1 = nb_perso;
-        mp.textContent = "Player 2 select your perso";
-    }else{
-        pl2 = nb_perso;
-        var sp = document.getElementById('select_player');
-        sp.style.display = 'none';
-        var ss = document.getElementById('select_stage');
-        ss.style.display = 'block';
+      var _class_selection = document.getElementsByClassName("selection")[0],
+        _id = _class_selection.getAttribute('id'),
+        _id_perso = document.getElementById(_id);
+    _id_perso_next = parseInt(_class_selection.getAttribute('id').charAt(1)) - 1;
+    console.log(_id_perso);
+    if (_id_perso_next >= 1){
+        var _perso_next = document.getElementById('p' + _id_perso_next);
+        _id_perso.classList.remove("selection");
+        _id_perso.classList.add("noselection");
+        _perso_next.classList.remove("noselection");
+        _perso_next.classList.add("selection");
     }
-    console.log(pl1 + ' ' + pl2);
-}
-
-function enter(){
-    var _s = document.getElementById("selection");
-    var _s_left = get_off_left(document.getElementById("selection"));
-    console.log(_s_left);
-    if (_s_left == 225)
-        p_select(1);
-    else if (_s_left == 348)
-        p_select(2);
-    else if (_s_left == 471)
-        p_select(3);
-    else if (_s_left == 771)
-        p_select(4);
-    else if (_s_left == 891)
-        p_select(5);
-    else if (_s_left == 1011)
-        p_select(6);
-}
+    
+    
+}  
 
 function move_selector(e){
     console.log(e.keyCode);
@@ -135,13 +88,12 @@ function move_selector(e){
         down();*/
     if (e.keyCode == 39)
         right();
-    else if (e.keyCode == 38)
-        up();
+    /*else if (e.keyCode == 38)
+        up();*/
     else if (e.keyCode == 37)
         left();
     else if (e.keyCode == 13)
         enter();
 }
 
-var sp = document.getElementById("select_player");
 addEventListener('keydown', move_selector);
